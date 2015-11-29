2d59f5da12bae65e518754b1badc18b81d4c68e1
tpphu <vietean@gmail.com>
2015-11-29 20:33:56 +0700

    Binary search with recursive version

--- binary_search.py
+++ binary_search.py
@@ -15,7 +15,21 @@ def binarySearch(alist, item):
             last = mid - 1
     return found
 
+def binarySearchWithRecursive(alist, item, first=0, last=None):
+    last = len(alist) - 1 if last is None else last
+    mid = (first + last)/2;
+    if first > last:
+        return -1
+    if alist[mid] == item:
+        return mid
+    elif item > alist[mid]:
+        return binarySearchWithRecursive(alist, item, mid + 1, last)
+    else:
+        return binarySearchWithRecursive(alist, item, first, mid -1)
+
+    
+
 #a list be sorted by asc
-alist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-print 'Found indexOf at: ', binarySearch(alist, 3)
+alist = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12]
+print 'Found indexOf at: ', binarySearchWithRecursive(alist, 5)
 
