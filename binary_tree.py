be99c505564a0cc82eb560944e36f7db97fd3d4f
tpphu <vietean@gmail.com>
2015-12-02 00:08:55 +0700

    Try to add binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -12,19 +12,38 @@ class Tree:
         return self.root
 
     def add(self, val):
-        if(self.root == None):
+        if self.root is None:
             self.root = Node(val)
         else:
             self._add(val, self.root)
 
     def _add(self, val, node):
-        if(val < node.v):
-            if(node.l != None):
-                self._add(val, node.l)
-            else:
+        if val < node.v:
+            if node.l is None:
                 node.l = Node(val)
-        else:
-            if(node.r != None):
-                self._add(val, node.r)
             else:
+                self._add(val, node.l)
+        else:
+            if node.r is None:
                 node.r = Node(val)
+            else:
+                self._add(val, node.r)
+
+    def printTree(self):
+        node = self.getRoot()
+        self._printTree(node)
+
+    def _printTree(self, node):
+        if node is None:
+            return
+        print node.v
+        self._printTree(node.l)
+        self._printTree(node.r)
+
+tree = Tree()
+tree.add(1)
+tree.add(2)
+tree.add(3)
+
+tree.printTree()
+        
